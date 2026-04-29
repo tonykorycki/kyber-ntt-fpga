1f0a8e7fbab05558c14f824c146aa4a4b7510fec
tonykorycki <koryckiantoni@gmail.com>
2026-04-28 22:02:18 -0400

    wired all files to top

--- hls/src/mul_ntt.h
+++ hls/src/mul_ntt.h
@@ -4,7 +4,8 @@
 
 #include "ntt_engine.h"
 
-// Base-case quadratic slot multiply: C[i] = A[i] * B[i] mod q  (eq. 17 of derivation)
+// Multiply two NTT-domain polynomials: applies BaseCaseMultiply to all N/2 quadratic slots.
+// Each slot i: (A[2i] + A[2i+1]*x) * (B[2i] + B[2i+1]*x) mod (x^2 - SLOT_ZETA[i])
 void mul_ntt(const coef_t A[N], const coef_t B[N], coef_t C[N]);
 
 #endif // MUL_NTT_H
