b7594ede0005b294fa877b0b6ce26ac590f2b1ba
tonykorycki <koryckiantoni@gmail.com>
2026-05-02 19:06:24 -0400

    feat: implemented the bram mux

--- rtl/bram_mux.sv
+++ rtl/bram_mux.sv
@@ -8,3 +8,32 @@
 //
 // This is the only RTL file with combinational logic.
 // All other .sv files are purely structural.
+
+module bram_mux #(
+    parameter   ADDR_W = 8,
+    parameter   DATA_W = 12
+)(
+    input logic              ps_own,
+
+    input logic              ps_en, hls_en,
+    input logic              ps_we, hls_we,
+    input logic [ADDR_W-1:0] ps_addr, hls_addr,
+    input logic [DATA_W-1:0] ps_din, hls_din,
+    input logic [DATA_W-1:0] bram_dout,
+
+    output logic              bram_en,
+    output logic              bram_we,
+    output logic [ADDR_W-1:0] bram_addr,
+    output logic [DATA_W-1:0] ps_dout, hls_dout,
+    output logic [DATA_W-1:0] bram_din
+);
+
+    assign bram_addr = ps_own ? ps_addr : hls_addr;
+    assign bram_din  = ps_own ? ps_din  : hls_din;
+    assign bram_we   = ps_own ? ps_we   : hls_we;
+    assign bram_en   = ps_own ? ps_en   : hls_en;
+
+    assign ps_dout   = bram_dout;
+    assign hls_dout  = bram_dout;
+    
+endmodule
